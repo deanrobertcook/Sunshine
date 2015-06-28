66b5935718fd76d6161eb901887c1978a57da3cc
deanrobertcook <deanrobertcook@gmail.com>
2015-06-28 11:43:11 +0200

    Added check to see which view type to use in ForecastAdapter

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -15,6 +15,11 @@ import com.example.android.sunshine.app.data.WeatherProjection;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_OTHER = 1;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -27,14 +32,35 @@ public class ForecastAdapter extends CursorAdapter {
         String tempString = Utility.formatTemperature(temp, isMetric);
         return tempString;
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_OTHER;
+    }
+
+    /*
+        We have two different types of views
+     */
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
     /*
         Remember that these views are reused as needed.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return view;
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else { //if (viewType == VIEW_TYPE_OTHER) {
+            layoutId = R.layout.list_item_forecast;
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
